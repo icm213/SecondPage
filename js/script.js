805db21d0be6a42fbdf224d08ccff52b86e846e3
icm213 <102415973+icm213@users.noreply.github.com>
2022-04-21 11:55:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,24 @@ for (let i = 0; i < gradientOff.length; i++) {
         grad[i].style.display = "none"
     }
 }
+const hamb = document.getElementsByClassName("nav-el-left")
+const hamb1 = document.getElementsByClassName("nav-el-right")
+const closeHamb1 = document.getElementById("close1")
+const closeHamb2 = document.getElementById("close2")
+const closeHamb3 = document.getElementById("close3")
+function displayNav() {
+    hamb[0].classList.toggle("display-nav")
+    hamb1[0].classList.toggle("display-nav")
+    closeHamb1.classList.toggle("close1")
+    closeHamb2.classList.toggle("close2")
+    closeHamb3.classList.toggle("close3")
+}
+const hambHide = document.getElementsByClassName("nav-el-left")
+const hambHide1 = document.getElementsByClassName("nav-el-right")
+function hideNav() {
+    hambHide[0].classList.remove("display-nav")
+    hambHide1[0].classList.remove("display-nav")
+    closeHamb1.classList.remove("close1")
+    closeHamb2.classList.remove("close2")
+    closeHamb3.classList.remove("close3")
+}
